929fc3e5000ae163686a145f70dd0f75d584ee3e
IMCG <almahkhanalmah@gmail.com>
2026-09-11 20:38:51 +0500

    Rename API_URL to SHEET_URL in script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const API_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQqgsr37FSz2HBLG_8kMAlGKly-GUxjFk3sOwqf0Z18KIZsMnUnWXTBIr4wUaAOlJLiL6vp1rDKmI5l/pub?output=csv";
-
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQqgsr37FSz2HBLG_8kMAlGKly-GUxjFk3sOwqf0Z18KIZsMnUnWXTBIr4wUaAOlJLiL6vp1rDKmI5l/pub?output=csv"
 function searchStudent() {
   const admissionNo = document.getElementById("admissionNo").value.trim();
   const resultDiv = document.getElementById("result");
